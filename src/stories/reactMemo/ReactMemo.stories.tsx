6865c2d595e401ba9fb23ada5460ae6305ce62a5
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-12-14 19:56:07 +0200

    commit 14.12

--- src/stories/reactMemo/ReactMemo.stories.tsx
+++ src/stories/reactMemo/ReactMemo.stories.tsx
@@ -1,5 +1,6 @@
 import {Meta, StoryObj} from "@storybook/react";
 import {ReactMemo} from "./ReactMemo";
+import {useState} from "react";
 
 const meta: Meta<typeof ReactMemo> = {
     component: ReactMemo,
@@ -10,7 +11,15 @@ type Story = StoryObj<typeof ReactMemo>;
 
 export const Ninestory: Story = {
     render:function Component(args) {
+        const [users, setUsers] = useState(["Oleg", "Ostap", "Anton", "Armagedon"])
+        const [count, setCount] = useState(0)
 
-        return <ReactMemo/>
+        return <>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <ReactMemo
+                count={count}
+                users={users}/>
+
+        </>
     }
 }
--- src/stories/reactMemo/ReactMemo.tsx
+++ src/stories/reactMemo/ReactMemo.tsx
@@ -1,4 +1,24 @@
+import reactMemoStories from "./ReactMemo.stories";
+import React from "react";
 
-export const ReactMemo =()=>{
-   return <div></div>
+export const ReactMemo = (props: { users: Array<any>, count: number }) => {
+    return <>
+        <Users users={props.users}/>
+        <NewMassagesCount count={props.count}/>
+    </>
+
+}
+
+const Users1 = (props: { users: Array<any> }) => {
+    return <div>
+        {props.users.map((el, i) => <div key={i}>{el}</div>)}
+
+    </div>
+
+}
+const Users = React.memo(Users1)
+const NewMassagesCount = (props: { count: number }) => {
+    return <div>
+        {props.count}
+    </div>
 }
